0833b5871d41ce5c943a716fbc2597de6f03be30
Bhushan Mokal <bhushanmokal02@gmail.com>
2022-08-04 15:58:34 +0530

    Initial Commit

---
+++ BusinessCardDemo/BusinessCardDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardDemoApp.swift
+//  BusinessCardDemo
+//
+//  Created by Neosoft on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
